d71f4b91a19736b4cb6c5b841f58473eb16519de
Tulsi Prasad <tulsi.prasad50@gmail.com>
2023-01-05 13:07:55 +0530

    feat: add google analytics to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Script from "next/script";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* After interactive is a loading strategy */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-${process.env.NEXT_PUBLIC_ANALYTICS_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
